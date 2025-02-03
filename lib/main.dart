759c2f92805f1810d41c3ed206599414e69e4b72
soul071213 <js071213asd@gmail.com>
2025-02-03 13:27:48 +0900

    feat : 버튼 모달 퍼블리싱 완료

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:bubble_v2/presentation/page/login/Login.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -10,9 +9,12 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home:LoginPage(),
+    return const MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Text('Hello World!'),
+        ),
+      ),
     );
   }
 }

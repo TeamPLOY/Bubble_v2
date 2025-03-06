e919b0af07bf734a2e0fbbbb265c1a003a97b4e2
soul071213 <js071213asd@gmail.com>
2025-03-06 17:18:16 +0900

    페이지 생성

---
+++ lib/presentation/page/noticeDetail.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class Noticedetail extends StatelessWidget {
+  final String title='제목';
+  final String date = "2022-12-12";
+  final String text = "호날두와 손흥민의 이야기 \n 킴펨베도 있음";
+
+  const Noticedetail({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
